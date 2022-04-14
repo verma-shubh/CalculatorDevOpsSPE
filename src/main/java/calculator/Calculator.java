b6628e15197b9118ef5660a11e4fa349731ca157
Shubham Verma <shubhamverma92@gmail.com>
2022-04-15 03:16:19 +0530

    Initital Commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,105 @@
+package calculator;
+
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
+    public Calculator() {
+    }
+
+    // Method for calculating square root of a number.
+    public double squareRoot(double input) {
+        logger.info("Calculating square root of: " + input + "...");
+        double result = Math.sqrt(input);
+        logger.info("Result: " + result);
+        return result;
+    }
+
+    // Method for calculating factorial of a number.
+    public double factorial(double input) {
+        logger.info("Calculating factorial of: " + input + "...");
+        if (input < 0) {
+            return Double.NaN;
+        } else {
+            double result = 1;
+            for (int i = 1; i <= input; i++) {
+                result = result * i;
+            }
+            logger.info("Result: " + result);
+            return result;
+        }
+    }
+
+    // Method for calculating natural logarithm of a number.
+    public double naturalLogarithm(double input) {
+        logger.info("Calculating natural logarithm of: " + input + "...");
+        double result = Math.log(input);
+        logger.info("Result: " + result);
+        return result;
+    }
+
+    // Method for calculating power of a given number.
+    public double exponentiation(double base, double power) {
+        logger.info("Calculating exponentiation of: " + base + " to the power of " + power + "...");
+        double result = Math.pow(base, power);
+        logger.info("Result: " + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        double input1, input2;
+        int choice;
+        while (true) {
+            System.out.println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+            System.out.println("Select one of the following operations:");
+            System.out.println("1. Square Root");
+            System.out.println("2. Factorial");
+            System.out.println("3. Natural Logarithm");
+            System.out.println("4. Power");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Selected Operation: Square Root.");
+                    System.out.print("Enter a number: ");
+                    input1 = scanner.nextDouble();
+                    System.out.println("Square root of " + input1 + " = " + calculator.squareRoot(input1));
+                    break;
+                case 2:
+                    System.out.println("Selected Operation: Factorial.");
+                    System.out.print("Enter a number: ");
+                    input1 = scanner.nextDouble();
+                    System.out.println("Factorial of " + input1 + " = " + calculator.factorial(input1));
+                    break;
+                case 3:
+                    System.out.println("Selected Operation: Natural Logarithm.");
+                    System.out.print("Enter a number: ");
+                    input1 = scanner.nextDouble();
+                    System.out.println("Factorial of " + input1 + " = " + calculator.naturalLogarithm(input1));
+                    break;
+                case 4:
+                    System.out.println("Selected Operation: Exponentiation.");
+                    System.out.print("Enter the base: ");
+                    input1 = scanner.nextDouble();
+                    System.out.print("Enter the exponent: ");
+                    input2 = scanner.nextDouble();
+                    System.out.println("Power of " + input1 + " raised to " + input2 + " = " + calculator.exponentiation(input1, input2));
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Enter a valid choice.");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,58 @@
+import calculator.Calculator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private static final double delta = 1e-15;
+    Calculator calculator = new Calculator();
+
+    //Tests for square root.
+    @Test
+    public void squareRootTruePositive(){
+        assertEquals("Calculating square root for true positive", 5, calculator.squareRoot(25), delta);
+        assertEquals("Calculating square root for true positive", 14, calculator.squareRoot(196), delta);
+    }
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals("Calculating square root for false positive", 6, calculator.squareRoot(20), delta);
+        assertNotEquals("Calculating square root for false positive", 11, calculator.squareRoot(76), delta);
+    }
+
+    //Tests for factorial.
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Calculating factorial for true positive", 120, calculator.factorial(5), delta);
+        assertEquals("Calculating factorial for true positive", 5040, calculator.factorial(7), delta);
+    }
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Calculating factorial for false positive", 16, calculator.factorial(4), delta);
+        assertNotEquals("Calculating factorial for false positive", 200, calculator.factorial(6), delta);
+    }
+
+    //Tests for natural logarithm.
+    @Test
+    public void naturalLogarithmTruePositive(){
+        assertEquals("Calculating natural logarithm for true positive", 0, calculator.naturalLogarithm(1), delta);
+        assertEquals("Calculating natural logarithm for true positive", Double.NaN, calculator.naturalLogarithm(3),
+                delta);
+    }
+    @Test
+    public void naturalLogarithmFalsePositive(){
+        assertNotEquals("Calculating square root for false positive", 1.25, calculator.naturalLogarithm(18), delta);
+        assertNotEquals("Calculating square root for false positive", 3.2, calculator.naturalLogarithm(56), delta);
+    }
+
+    @Test
+    public void exponentiationTruePositive(){
+        assertEquals("Calculating power for true positive", 25, calculator.exponentiation(5, 2), delta);
+        assertEquals("Calculating power for true positive", 343, calculator.exponentiation(7, 3), delta);
+    }
+    @Test
+    public void exponentiationFalsePositive(){
+        assertNotEquals("Calculating power for false positive", 5, calculator.exponentiation(2, 3), delta);
+        assertNotEquals("Calculating power for false positive", 50, calculator.exponentiation(8, 3), delta);
+    }
+
+
+}
